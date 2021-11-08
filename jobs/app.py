ac8c8622010b725b25bac7581872569cf75788bb
Aaron Gonzalez <aaronraulgonzalez@gmail.com>
2021-11-08 16:56:57 -0600

    Display Jobs

--- jobs/app.py
+++ jobs/app.py
@@ -1,8 +1,8 @@
-from sqlite3.dbapi2 import connect
-from flask import Flask, render_template, g
 import sqlite3
+from flask import Flask, render_template, g
 
 PATH = 'db/jobs.sqlite'
+
 app = Flask(__name__)
 
 def open_connection():
@@ -32,4 +32,5 @@ def close_connection(exception):
 @app.route('/')
 @app.route('/jobs')
 def jobs():
-    return render_template('index.html')
+    jobs = execute_sql('SELECT job.id, job.title, job.description, job.salary, employer.id as employer_id, employer.name as employer_name FROM job JOIN employer ON employer.id = job.employer_id')
+    return render_template('index.html', jobs=jobs)
